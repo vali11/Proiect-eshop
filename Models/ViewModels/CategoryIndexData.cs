136558ae0485289e4dc6ecbf59c80d4816075afd
valiv <valentin.voin@stud.ubbcluj.ro>
2023-01-09 14:03:52 +0200

    Sort products by category

---
+++ Models/ViewModels/CategoryIndexData.cs
@@ -0,0 +1,10 @@
+﻿namespace Proiect_eshop.Models.ViewModels
+{
+    public class CategoryIndexData
+    {
+        public IEnumerable<Category> Categories { get; set; }
+        public IEnumerable<ProductCategory> ProductCategories { get; set; }
+
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
